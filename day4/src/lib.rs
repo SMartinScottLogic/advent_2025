1c681980ad9ab9ef50580907bd8ca14538222adf
Simon Martin (He/Him) <smartin@scottlogic.com>
2025-12-04 05:26:13 +0000

    Day 4 complete

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -1,12 +1,19 @@
 use std::io::{BufRead, BufReader};
 #[allow(unused_imports)]
 use tracing::{debug, event_enabled, info, Level};
+use utils::grid::Matrix;
 
 pub type ResultType = u64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    matrix: Matrix<char>,
+}
+impl Solution {
+    pub fn set_matrix(&mut self, x: usize, y: usize, c: char) {
+        self.matrix.set(x as isize, y as isize, c);
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -14,8 +21,11 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
 
     fn try_from(reader: BufReader<T>) -> Result<Self, Self::Error> {
         let mut solution = Self::default();
-        for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
+        for (y, line) in reader.lines().map_while(Result::ok).enumerate() {
             // Implement for problem
+            for (x, c) in line.trim().chars().enumerate() {
+                solution.set_matrix(x, y, c);
+            }
         }
         Ok(solution)
     }
@@ -25,30 +35,50 @@ impl utils::Solution for Solution {
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        let count = get_removable(&self.matrix).len();
         // Implement for problem
-        Ok(0)
+        Ok(count as ResultType)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        let mut removed = 0;
+        let mut matrix = self.matrix.clone();
+        loop {
+            let removable = get_removable(&matrix);
+            if removable.is_empty() {
+                break;
+            }
+            removed += removable.len();
+            for (x, y) in removable {
+                matrix.set(x, y, '.');
+            }
+        }
         // Implement for problem
-        Ok(0)
+        Ok(removed as ResultType)
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::io::BufReader;
-
-    use tracing_test::traced_test;
-    use utils::Solution;
-
-    #[test]
-    #[traced_test]
-    fn read() {
-        let input = "replace for problem";
-        let r = BufReader::new(input.as_bytes());
-        let s = crate::Solution::try_from(r).unwrap();
-        assert_eq!(0 as ResultType, s.answer_part1(false).unwrap());
+fn get_removable(matrix: &Matrix<char>) -> Vec<(isize, isize)> {
+    let mut removable = Vec::new();
+    for y in matrix.min_y()..=matrix.max_y() {
+        for x in matrix.min_x()..=matrix.max_x() {
+            if let Some('@') = matrix.get(x, y) {
+                let mut neighbours = 0;
+                for dy in -1..=1 {
+                    for dx in -1..=1 {
+                        if dx == 0 && dy == 0 {
+                            continue;
+                        }
+                        if let Some('@') = matrix.get(x + dx, y + dy) {
+                            neighbours += 1;
+                        }
+                    }
+                }
+                if neighbours < 4 {
+                    removable.push((x, y));
+                }
+            }
+        }
     }
+    removable
 }
