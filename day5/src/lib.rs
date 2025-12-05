1c42d2c8fe8d444e8b891128111ff128b89fb1bd
Simon Martin (He/Him) <smartin@scottlogic.com>
2025-12-05 05:47:11 +0000

    Day 5 complete

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -5,8 +5,18 @@ use tracing::{debug, event_enabled, info, Level};
 pub type ResultType = u64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    freshlist: Vec<(u64, u64)>,
+    ingredients: Vec<u64>,
+}
+impl Solution {
+    pub fn add_ingredient(&mut self, ingredient: u64) {
+        self.ingredients.push(ingredient);
+    }
+    pub fn add_freshlist(&mut self, start: u64, end: u64) {
+        self.freshlist.push((start, end));
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -16,6 +26,21 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
         let mut solution = Self::default();
         for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
             // Implement for problem
+            let line = line.trim();
+            if line.is_empty() {
+                continue;
+            }
+            match line.split_once('-') {
+                Some((a, b)) => {
+                    let start = a.parse().unwrap();
+                    let end = b.parse().unwrap();
+                    solution.add_freshlist(start, end);
+                }
+                None => {
+                    let ingredient = line.parse().unwrap();
+                    solution.add_ingredient(ingredient);
+                }
+            }
         }
         Ok(solution)
     }
@@ -25,30 +50,69 @@ impl utils::Solution for Solution {
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        let mut count = 0;
+        for ingredient in &self.ingredients {
+            if is_fresh(*ingredient, &self.freshlist) {
+                count += 1;
+            }
+        }
         // Implement for problem
-        Ok(0)
+        Ok(count)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        let mut ranges = Vec::new();
+        for (start, end) in &self.freshlist {
+            ranges = add_range(&ranges, *start, *end);
+        }
+        let mut count = 0;
+        for (start, end) in ranges.iter() {
+            count += 1 + end - start;
+        }
+        debug!(?ranges, count);
         // Implement for problem
-        Ok(0)
+        Ok(count as ResultType)
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::io::BufReader;
+fn is_fresh(ingredient: u64, freshlist: &[(u64, u64)]) -> bool {
+    freshlist
+        .iter()
+        .any(|(start, end)| (*start..=*end).contains(&ingredient))
+}
 
-    use tracing_test::traced_test;
-    use utils::Solution;
-
-    #[test]
-    #[traced_test]
-    fn read() {
-        let input = "replace for problem";
-        let r = BufReader::new(input.as_bytes());
-        let s = crate::Solution::try_from(r).unwrap();
-        assert_eq!(0 as ResultType, s.answer_part1(false).unwrap());
+fn add_range(ranges: &[(u64, u64)], start: u64, end: u64) -> Vec<(u64, u64)> {
+    let mut new_ranges = Vec::new();
+    let mut overlaps = Vec::new();
+    for (rs, re) in ranges.iter() {
+        if start < *rs && end < *rs {
+            new_ranges.push((*rs, *re));
+            continue;
+        }
+        if start > *re && end > *re {
+            new_ranges.push((*rs, *re));
+            continue;
+        }
+        // Must overlap
+        debug!("overlap ({} {}) ({} {})", *rs, *re, start, end);
+        overlaps.push((start, end));
+        overlaps.push((*rs, *re));
     }
+    if overlaps.is_empty() {
+        debug!(start, end, "new");
+        new_ranges.push((start, end));
+    } else {
+        let mut start = start;
+        let mut end = end;
+        debug!(
+            "no_overlaps {:?}; overlaps {:?} {} {}",
+            new_ranges, overlaps, start, end
+        );
+        for (os, oe) in overlaps.into_iter() {
+            start = std::cmp::min(start, os);
+            end = std::cmp::max(end, oe);
+        }
+        new_ranges.push((start, end));
+    }
+    new_ranges
 }
