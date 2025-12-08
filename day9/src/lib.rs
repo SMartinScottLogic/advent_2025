780819a1a993a8fe814d55f8befd0e507bda44f2
Simon Martin (He/Him) <smartin@scottlogic.com>
2025-12-08 07:10:34 +0000

    Day 9 init

---
+++ day9/src/lib.rs
@@ -0,0 +1,54 @@
+use std::io::{BufRead, BufReader};
+#[allow(unused_imports)]
+use tracing::{debug, event_enabled, info, Level};
+
+pub type ResultType = u64;
+
+#[derive(Debug, Default)]
+pub struct Solution {}
+impl Solution {}
+
+#[allow(unused_variables, unused_mut)]
+impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
+    type Error = std::io::Error;
+
+    fn try_from(reader: BufReader<T>) -> Result<Self, Self::Error> {
+        let mut solution = Self::default();
+        for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
+            // Implement for problem
+        }
+        Ok(solution)
+    }
+}
+impl utils::Solution for Solution {
+    type Result = anyhow::Result<ResultType>;
+    fn analyse(&mut self, _is_full: bool) {}
+
+    fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        // Implement for problem
+        Ok(0)
+    }
+
+    fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        // Implement for problem
+        Ok(0)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::BufReader;
+
+    use tracing_test::traced_test;
+    use utils::Solution;
+
+    #[test]
+    #[traced_test]
+    fn read() {
+        let input = "replace for problem";
+        let r = BufReader::new(input.as_bytes());
+        let s = crate::Solution::try_from(r).unwrap();
+        assert_eq!(0 as ResultType, s.answer_part1(false).unwrap());
+    }
+}
