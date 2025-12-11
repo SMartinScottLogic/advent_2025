7c6ff81b6105fd36e7130398db08818a31581eaf
Simon Martin <smartin@scottlogic.com>
2025-12-11 05:20:01 +0000

    Day 11 part 1 complete

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -1,12 +1,21 @@
-use std::io::{BufRead, BufReader};
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader},
+};
 #[allow(unused_imports)]
 use tracing::{debug, event_enabled, info, Level};
 
 pub type ResultType = u64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    devices: HashMap<String, Vec<String>>,
+}
+impl Solution {
+    pub(crate) fn add_device(&mut self, source: String, targets: Vec<String>) {
+        self.devices.insert(source, targets);
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -16,6 +25,14 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
         let mut solution = Self::default();
         for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
             // Implement for problem
+            let (source, targets) = line.split_once(':').unwrap();
+            let source = source.trim().to_string();
+            let targets = targets
+                .split(' ')
+                .filter(|s| !s.is_empty())
+                .map(|s| s.to_string())
+                .collect::<Vec<_>>();
+            solution.add_device(source, targets);
         }
         Ok(solution)
     }
@@ -24,31 +41,36 @@ impl utils::Solution for Solution {
     type Result = anyhow::Result<ResultType>;
     fn analyse(&mut self, _is_full: bool) {}
 
-    fn answer_part1(&self, _is_full: bool) -> Self::Result {
+    fn answer_part1(&self, is_full: bool) -> Self::Result {
+        let r = if is_full {
+            num_paths(&self.devices, "you", "out")
+        } else {
+            0
+        };
         // Implement for problem
-        Ok(0)
+        Ok(r as ResultType)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        let r = num_paths(&self.devices, "svr", "dac");
         // Implement for problem
-        Ok(0)
+        Ok(r as ResultType)
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::io::BufReader;
-
-    use tracing_test::traced_test;
-    use utils::Solution;
-
-    #[test]
-    #[traced_test]
-    fn read() {
-        let input = "replace for problem";
-        let r = BufReader::new(input.as_bytes());
-        let s = crate::Solution::try_from(r).unwrap();
-        assert_eq!(0 as ResultType, s.answer_part1(false).unwrap());
+fn num_paths(devices: &HashMap<String, Vec<String>>, current: &str, target: &str) -> usize {
+    if current == target {
+        1
+    } else {
+        match devices.get(current) {
+            None => 0,
+            Some(targets) => {
+                let mut count = 0;
+                for next in targets {
+                    count += num_paths(devices, next, target);
+                }
+                count
+            }
+        }
     }
 }
